39b92d48aefbdbd2a7ae3790697ca5730ddfd179
Lorenzo Rigato <lore91tanz@gmail.com>
2013-12-05 17:34:10 +0100

    created new repo

---
+++ src/introsde/ws/HelloWorld.java
@@ -0,0 +1,31 @@
+package introsde.ws;
+
+import javax.jws.WebMethod;
+
+import javax.jws.WebParam;
+
+import javax.jws.WebResult;
+
+import javax.jws.WebService;
+
+import javax.jws.soap.SOAPBinding;
+
+import javax.xml.ws.Action;
+
+@WebService(name = "HelloWorld", targetNamespace = "http://ws.introsde/")
+
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+
+public interface HelloWorld {
+
+ @WebMethod
+
+ @WebResult(partName = "return")
+
+ @Action(input = "http://ws.introsde/HelloWorld/getHelloWorldAsStringRequest", 
+
+ output = "http://ws.introsde/HelloWorld/getHelloWorldAsStringResponse")
+
+ public String getHelloWorldAsString(@WebParam(name = "arg0", partName = "arg0") String arg0);
+
+}
